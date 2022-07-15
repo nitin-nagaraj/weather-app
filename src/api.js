11c81f155ac8d457d6de5eef2cbeeb3319ab357a
Nitin Nagaraj <nnitin91@gmail.com>
2022-07-16 00:02:55 +0530

    current weather done

--- src/api.js
+++ src/api.js
@@ -12,3 +12,6 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "dfc8c83491a434239f0a327f7a9c12aa";
