914e7b9e6e6615e2288e0d93922cfb94c1e362d2
Nitin Nagaraj <nnitin91@gmail.com>
2022-07-16 02:29:02 +0530

    all functionality built

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Search from "./components/search/search";
+import Forecast from "./components/forecast/forecast";
 import CurrentWeather from "./components/current-weather/current-weather";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import { useState } from "react";
@@ -29,13 +30,11 @@ function App() {
       .catch((err) => console.log(err));
   };
 
-  
-  console.log(forecast);
-
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
